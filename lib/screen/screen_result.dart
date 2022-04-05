58c0365a141710464a27155d55002c466d442aa6
yeyoung8805 <yyk8805@naver.com>
2022-04-05 16:28:32 +0900

    edit quiz result screen(and make back key not to work)

--- lib/screen/screen_result.dart
+++ lib/screen/screen_result.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz_app/model/model_quiz.dart';
+import 'package:flutter_quiz_app/screen/screen_home.dart';
 
 class ResultScreen extends StatelessWidget {
   List<int> answers;
   List<Quiz> quizs;
+
   ResultScreen({required this.answers, required this.quizs});
 
   @override
@@ -14,14 +16,16 @@ class ResultScreen extends StatelessWidget {
 
     int score = 0;
     //반복문으로 퀴즈의 정답을 체크하여 score 을 계산한다.
-    for(int i=0; i <quizs.length; i++) {
-      if(quizs[i].answer == answers[i]) {
+    for (int i = 0; i < quizs.length; i++) {
+      if (quizs[i].answer == answers[i]) {
         score += 1; //일치하면 점수를 1점 올린다.
       }
     }
 
     //SafeArea에 Scaffold넣는 방식으로 화면을 구성한다.
-    return SafeArea(
+    return WillPopScope(
+      onWillPop: () async => false, //기기의 back 버튼으로 돌아가는 것이 수행 안된다.
+        child: SafeArea(
       child: Scaffold(
         appBar: AppBar(
           title: Text('My Quiz App'),
@@ -31,20 +35,90 @@ class ResultScreen extends StatelessWidget {
         body: Center(
           child: Container(
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              border: Border.all(color: Colors.deepPurple),
-              color: Colors.deepPurple
-            ),
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.deepPurple),
+                color: Colors.deepPurple),
             width: width * 0.85,
             height: height * 0.5,
             child: Column(
               children: <Widget>[
-
+                Padding(
+                  padding: EdgeInsets.only(bottom: width * 0.048),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.deepPurple),
+                    color: Colors.white,
+                  ),
+                  width: width * 0.73,
+                  height: height * 0.25,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.fromLTRB(
+                            0, width * 0.048, 0, width * 0.012),
+                        child: Text(
+                          '수고하셨습니다!',
+                          style: TextStyle(
+                            fontSize: width * 0.055,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '당신의 점수는',
+                        style: TextStyle(
+                          fontSize: width * 0.048,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Text(
+                        score.toString() + '/' + quizs.length.toString(),
+                        style: TextStyle(
+                          fontSize: width * 0.21,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
+                        ),
+                      ),
+                      // Padding( //android emulator 에서는 잘림..이 기능 넣으려면 추가작업 필요할것 같다!
+                      //   padding: EdgeInsets.all(width * 0.012),
+                      // )
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Container(),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: width * 0.048),
+                  child: ButtonTheme(
+                    minWidth: width * 0.73,
+                    height: height * 0.05,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return HomeScreen();
+                        }));
+                      },
+                      child: Text('홈으로 돌아가기'),
+                      color: Colors.white,
+                      textColor: Colors.black,
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
         ),
       ),
-    );
+    ));
   }
 }
